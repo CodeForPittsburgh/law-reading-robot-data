258213e37d0e6aebebdf4d5bb9e9ba1841b1d3be
Chris Palmer <capalmer1013@gmail.com>
2023-04-29 22:34:17 -0400

    added migrations

--- LRRServer/lawreadingrobot/database.py
+++ LRRServer/lawreadingrobot/database.py
@@ -1,9 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///./lrr.db')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
--- LRRServer/lawreadingrobot/main.py
+++ LRRServer/lawreadingrobot/main.py
@@ -36,5 +36,15 @@ def get_db() -> Generator[Session, None, None]:
 
 
 @app.get("/housebill/", response_model=list[schemas.HouseBillGet])
-def get_housebill(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> List[models.HouseBill]:
+def get_housebills(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> List[models.HouseBill]:
     return crud.get_housebills(db, skip=skip, limit=limit)
+
+
+@app.get("/housebill/{housebill_id}", response_model=schemas.HouseBillGet)
+def get_housebill(housebill_id: str, db: Session = Depends(get_db)) -> models.HouseBill:
+    return crud.get_housebill(db, housebill_id=housebill_id)
+
+
+@app.post("/housebill/", response_model=schemas.HouseBillGet)
+def create_housebill(housebill: schemas.HouseBillCreate, db: Session = Depends(get_db)) -> models.HouseBill:
+    return crud.create_housebill(db=db, housebill=housebill)
