4e25cbd3f5b2c1d9068493980aa3780652fb965e
Michael DePace <michaeldepacejr@gmail.com>
2023-08-08 12:29:15 -0400

    cleaned up files, etl workflow, web scraper

--- src/web_scraper/main.py
+++ src/web_scraper/main.py
@@ -1,52 +1,56 @@
-import csv
-import feedparser
+#
+# old code to be removed
+#
 
-# Dictionary listing the name of the csv file and the URL of the RSS feed
-feeds = {
-    'HouseBillsAndResolutions': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseBills.xml',
-    'SenateBillsAndResolutions': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateBills.xml',
-    'HouseSessionCalendar': 'https://www.legis.state.pa.us/WU01/LI/RSS/CAL/HouseCalendarSS0reg.xml',
-    'SenateSessionCalendar': 'https://www.legis.state.pa.us/WU01/LI/RSS/CAL/SenateCalendarSS0reg.xml',
-    'HouseDailySessionReports': 'https://www.legis.state.pa.us/WU01/LI/RSS/DailySessionReports.xml',
-    'SenateSessionNotes': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateNotes.xml',
-    'HouseCommitteeMeetingSchedule': 'https://www.legis.state.pa.us/WU01/LI/RSS/CMSH.xml',
-    'SenateCommitteeMeetingSchedule': 'https://www.legis.state.pa.us/WU01/LI/RSS/CMSS.xml',
-    'HouseLegislativeJournals': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseJournals.xml',
-    'SenateLegislativeJournals': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateJournals.xml',
-    'HouseRollCallVotes': 'https://www.legis.state.pa.us/WU01/LI/RSS/HRC.xml',
-    'SenateRollCallVotes': 'https://www.legis.state.pa.us/WU01/LI/RSS/SRC.xml',
-    'HouseVotedAmendments': 'https://www.legis.state.pa.us/WU01/LI/RSS/FA/HouseAmendments.xml',
-    'SenateFloorAmendments': 'https://www.legis.state.pa.us/WU01/LI/RSS/FA/SenateAmendments.xml',
-    'HouseCoSponsorshipMemoranda': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseCosponsorship.xml',
-    'SenateCoSponsorshipMemoranda': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateCosponsorship.xml',
-    'HouseMembers': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseMembers.xml',
-    'SenateMembers': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateMembers.xml',
-    'HouseCommitteeAssignments': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseCommitteeAssignments.xml',
-    'SenateCommitteeAssignments': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateCommitteeAssignments.xml',
-    'SenateExecutiveNominationsCalendar': 'https://www.legis.state.pa.us/WU01/LI/RSS/NominationsCalendar.xml',
-}
+# import csv
+# import feedparser
 
-for name, url in feeds.items():
-    # Parse the RSS feed
-    feed = feedparser.parse(url)
+# # Dictionary listing the name of the csv file and the URL of the RSS feed
+# feeds = {
+#     'HouseBillsAndResolutions': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseBills.xml',
+#     'SenateBillsAndResolutions': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateBills.xml',
+#     'HouseSessionCalendar': 'https://www.legis.state.pa.us/WU01/LI/RSS/CAL/HouseCalendarSS0reg.xml',
+#     'SenateSessionCalendar': 'https://www.legis.state.pa.us/WU01/LI/RSS/CAL/SenateCalendarSS0reg.xml',
+#     'HouseDailySessionReports': 'https://www.legis.state.pa.us/WU01/LI/RSS/DailySessionReports.xml',
+#     'SenateSessionNotes': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateNotes.xml',
+#     'HouseCommitteeMeetingSchedule': 'https://www.legis.state.pa.us/WU01/LI/RSS/CMSH.xml',
+#     'SenateCommitteeMeetingSchedule': 'https://www.legis.state.pa.us/WU01/LI/RSS/CMSS.xml',
+#     'HouseLegislativeJournals': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseJournals.xml',
+#     'SenateLegislativeJournals': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateJournals.xml',
+#     'HouseRollCallVotes': 'https://www.legis.state.pa.us/WU01/LI/RSS/HRC.xml',
+#     'SenateRollCallVotes': 'https://www.legis.state.pa.us/WU01/LI/RSS/SRC.xml',
+#     'HouseVotedAmendments': 'https://www.legis.state.pa.us/WU01/LI/RSS/FA/HouseAmendments.xml',
+#     'SenateFloorAmendments': 'https://www.legis.state.pa.us/WU01/LI/RSS/FA/SenateAmendments.xml',
+#     'HouseCoSponsorshipMemoranda': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseCosponsorship.xml',
+#     'SenateCoSponsorshipMemoranda': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateCosponsorship.xml',
+#     'HouseMembers': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseMembers.xml',
+#     'SenateMembers': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateMembers.xml',
+#     'HouseCommitteeAssignments': 'https://www.legis.state.pa.us/WU01/LI/RSS/HouseCommitteeAssignments.xml',
+#     'SenateCommitteeAssignments': 'https://www.legis.state.pa.us/WU01/LI/RSS/SenateCommitteeAssignments.xml',
+#     'SenateExecutiveNominationsCalendar': 'https://www.legis.state.pa.us/WU01/LI/RSS/NominationsCalendar.xml',
+# }
 
-    # If the feed has no entries, skip it
-    if len(feed.entries) == 0:
-        continue
+# for name, url in feeds.items():
+#     # Parse the RSS feed
+#     feed = feedparser.parse(url)
 
-    # Get the child keys from the first item in the feed
-    # We assume that all items in the feed have the same child keys
-    keys = list(feed.entries[0].keys())
+#     # If the feed has no entries, skip it
+#     if len(feed.entries) == 0:
+#         continue
 
-    # Open a new CSV file for writing
-    with open('rss_data/' + name + '.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
+#     # Get the child keys from the first item in the feed
+#     # We assume that all items in the feed have the same child keys
+#     keys = list(feed.entries[0].keys())
 
-        # Write the header row
-        writer.writerow(keys)
+#     # Open a new CSV file for writing
+#     with open('rss_data/' + name + '.csv', 'w', newline='') as csvfile:
+#         writer = csv.writer(csvfile)
 
-        # Write the data rows
-        for entry in feed.entries:
-            row = [entry.get(key, '') for key in keys]
-            writer.writerow(row)
+#         # Write the header row
+#         writer.writerow(keys)
+
+#         # Write the data rows
+#         for entry in feed.entries:
+#             row = [entry.get(key, '') for key in keys]
+#             writer.writerow(row)
 
--- src/web_scraper/web_scraper.py
+++ src/web_scraper/web_scraper.py
@@ -1,118 +1,124 @@
-#this program will eventually be responsible for grabbing bill metadata and full bill text to supabase
+"""
+
+Description: this program will eventually be responsible for extracting RSS senate/house bill metadata and DOCX full bill, and then uploading to supabase table
+
+
+"""
 
 from docx import Document
 from supabase import create_client, Client
 from datetime import datetime
 import feedparser
+import os
+import re
 
 senate_rss_feed = "https://www.legis.state.pa.us/WU01/LI/RSS/SenateBills.xml"
 house_rss_feed = "https://www.legis.state.pa.us/WU01/LI/RSS/HouseBills.xml"
 
-#what should I do with the api key (shouldn't it be a secret or something)
-sb_api_url = "https://vsumrxhpkzegrktbtcui.supabase.co"
-sb_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzdW1yeGhwa3plZ3JrdGJ0Y3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ3OTU2NzUsImV4cCI6MjAwMDM3MTY3NX0.9lafalZT9FJW1D8DAuIMrsRX0Gs6204nV8ETfGslrqI"
+sb_api_url = "https://vsumrxhpkzegrktbtcui.supabase.co" #os.environ["API_URL"] 
+sb_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzdW1yeGhwa3plZ3JrdGJ0Y3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ3OTU2NzUsImV4cCI6MjAwMDM3MTY3NX0.9lafalZT9FJW1D8DAuIMrsRX0Gs6204nV8ETfGslrqI" #os.environ["API_KEY"] 
 
-#on each post, check if the revision is already in the table (so you don't upload duplicates)
-def extract_and_upload_senate_bill_metadata(): #research how to extract rss metadata and what rss is 
+def extract_and_upload_senate_bill_metadata():
     sen_rss = feedparser.parse(senate_rss_feed)
     supabase: Client = create_client(sb_api_url, sb_api_key)
-
     for bill in sen_rss.entries:
-        # if exists
-        if bill is None: return
+        #print("checking bill ", bill.title)
+        if bill is None: continue
 
-        title_elements = bill.title.split(' ')
-        #change if bill["parss_enacted"] == "YES" and title_elements[1] == "Bill": #make sure the bill was enacted and it's a bill instead of a resolution
-            
-        bill_no = title_elements[2]
-        printer_no = title_elements[5]
+        bill_no_regex = '(Senate Bill \d*|Senate Resolution \d*)'
+        bill_no = re.findall(bill_no_regex, bill.title)[0].split(' ')[-1]
+        
+        printer_no_regex="Printer's Number \d*"
+        printer_no = re.findall(printer_no_regex, bill.title)[0].split(' ')[-1]
 
-        bill_record = supabase.table('Revisions').select("*", count="exact").eq("bill_id", bill_no).eq("printer_no", printer_no).execute()
-        if bill_record.count == 0: #if the bill hasn't already been posted to the table, then post it
-            response = supabase.table('Revisions').upsert( #why is there no python docs for upsert?
+        #OLD CODE that doesn't work
+        #title_elements = bill.title.split(' ') #grabbing bill and printer numbers from the title (not explicitly given in RSS feed)
+        #bill_no = "".join(char for char in title_elements[2] if char.isdigit()) #ensuring that only the actual digits of the bill number are extracted
+        #printer_no = "".join(char for char in title_elements[5] if char.isdigit()) #ensuring that only the actual digits of the printer number are extracted
+
+        #change if bill["parss_enacted"] == "YES" and title_elements[1] == "Bill": #make sure the bill was enacted and it's a bill instead of a resolution        
+        bill_existing_records = supabase.table('Revisions').select("*", count="exact").eq("bill_id", bill_no).eq("printer_no", printer_no).eq("legislative_body", "senate").execute()
+        if bill_existing_records.count == 0: #if the bill hasn't already been recorded in the supabase table, then post it (so you don't upload duplicates)
+            response = supabase.table('Revisions').insert( #why is there no python docs for upsert?
                 {
-                    # "created_at": metadata["created_at"],
+                    # "created_at": metadata["created_at"], #supabase already defaults to now(), so this is kind of unnecessary for now
                     "bill_id": bill_no,
                     "printer_no": printer_no, 
                     "full_text_link": bill["link"], 
                     "publication_date": bill["published"], 
                     "legislative_body": "senate"
-                    # "active_summary_id": metadata["active_summary_id"], 
+                    # "active_summary_id": metadata["active_summary_id"], #leaving out foreign keys for now
                 }).execute()
+            print(response)
     return
 
-def extract_and_upload_house_bill_metadata(): #research how to extract rss metadata and what rss is 
+def extract_and_upload_house_bill_metadata():
     house_rss = feedparser.parse(house_rss_feed)
     supabase: Client = create_client(sb_api_url, sb_api_key)
-
     for bill in house_rss.entries:
-        # if exists
-        if bill is None: return
+        #print("checking bill ", bill.title)
+        if bill is None: continue
 
-        title_elements = bill.title.split(' ')
+        bill_no_regex = '(House Bill \d*|House Resolution \d*)'
+        bill_no = re.findall(bill_no_regex, bill.title)[0].split(' ')[-1]
+
+        printer_no_regex="Printer's Number \d*"
+        printer_no = re.findall(printer_no_regex, bill.title)[0].split(' ')[-1]
+
+        #old code that doesn't work
+        #title_elements = bill.title.split(' ')#grabbing bill and printer numbers from the title (not explicitly given in RSS feed)
+        #bill_no = "".join(char for char in title_elements[2] if char.isdigit()) #ensuring that only the actual digits of the bill number are extracted
+        #printer_no = "".join(char for char in title_elements[5] if char.isdigit()) #ensuring that only the actual digits of the printer number are extracted
+
         #if bill["parss_enacted"] == "YES" and title_elements[1] == "Bill": #make sure the bill was enacted and it's a bill instead of a resolution
-            
-        bill_no = title_elements[2]
-        printer_no = title_elements[5]
-
-        bill_record = supabase.table('Revisions').select("*", count="exact").eq("bill_id", bill_no).eq("printer_no", printer_no).execute()
-        if bill_record.count == 0: #if the bill hasn't already been posted to the table, then post it
-            response = supabase.table('Revisions').upsert( #why is there no python docs for upsert?
+        bill_record = supabase.table('Revisions').select("*", count="exact").eq("bill_id", bill_no).eq("printer_no", printer_no).eq("legislative_body", "house").execute()
+        if bill_record.count == 0: #if the bill hasn't already been recorded in the supabase table, then post it (so you don't upload duplicates)
+            response = supabase.table('Revisions').insert( #why is there no python docs for upsert?
                 {
-                    # "created_at": metadata["created_at"],
+                    # "created_at": metadata["created_at"], #supabase already defaults to now(), so this is kind of unnecessary for now
                     "bill_id": bill_no,
                     "printer_no": printer_no, 
                     "full_text_link": bill["link"], 
                     "publication_date": bill["published"], 
                     "legislative_body": "house"
-                    # "active_summary_id": metadata["active_summary_id"], 
+                    # "active_summary_id": metadata["active_summary_id"], # "active_summary_id": metadata["active_summary_id"], #leaving out foreign keys for now
                 }).execute()
     return
 
-#extract_law_text(path)
-# input: filepath of docx file
-# output: a string of the law body text
-def extract_law_text(path):
-    output_text = ""
-    doc = Document(path)
+#
+# THE COMMENTED OUT CODE BELOW WORKS, but is on hold until the doc to docx conversion process has been implemented
+#
 
-    for para in doc.paragraphs:
-        for run in para.runs: #not sure what runs are, but these for loops seem to look through on a character by character basis (testing by strikethroughing a single character and this picks it up as an individual run)
-            if not run.font.strike: #only extract text that isn't strikethrough 
-                output_text += run.text
-        output_text += ' ' #resolves spacing issues
-    return output_text
+"""
+looked into temorarily storing in vm, then manipulating files within an actions runner
+you can save to the home directory as shown here: https://medium.com/@fonseka.live/sharing-data-in-github-actions-a9841a9a6f42
+how this is actually implemented will dramatically impact how my extract law text function works
+"""
 
-#find a way to determine when the actual bill text starts
-#do I need to validate that it's a docx file
+def extract_law_text(filepath):
+    # output_text = ""
+    # doc = Document(filepath)
 
-def upload_to_supabase(metadata, law_text):
-#connect to and post to a test supabase table
-    supabase: Client = create_client(sb_api_url, sb_api_key)
+    # #update to only include docs that haven't been posted yet
+    #find a way to determine when the actual bill text starts
+    #do I need to validate that it's a docx file
 
-    #supabase 2 part update or insert (1 for metadata and the other for the text?)
 
-    response = supabase.table('Revisions').upsert( #why is there no python docs for upsert?
-        {
-            "created_at": metadata["created_at"],
-            "bill_id": metadata["bill_id"],
-            "printer_no": metadata["printer_no"], 
-            "full_text_link": metadata["full_text_link"], 
-            "publication_date": metadata["publication_date"], 
-            # "active_summary_id": metadata["active_summary_id"], 
-            "full_text": law_text
-        }).execute()
-    # print(response.data[0])
+    # for para in doc.paragraphs:
+    #     for run in para.runs: #not sure what runs are, but these for loops seem to look through on a character by character basis (testing by strikethroughing a single character and this picks it up as an individual run)
+    #         if not run.font.strike: #only extract text that isn't strikethrough 
+    #             output_text += run.text
+    #     output_text += ' ' #resolves spacing issues
+    # return output_text
+    return
+
+def process_docx_files(): #function to handle the scraping and uploading of all docx files
+    # get all bills from revsions withe empty full_text fields
+    # for each bill, post
     return
 
 
-
-
-#upload_to_supabase("", extract_law_text('./eg-doc-106.docx'))
-
-# print(extract_law_text('./eg-doc-106.docx'))
-# print()
-# print(extract_law_text('./HB-366.docx'))
-
-#extract_and_upload_senate_bill_metadata()
-#extract_and_upload_house_bill_metadata()
+if __name__ == "__main__":
+    extract_and_upload_senate_bill_metadata()
+    extract_and_upload_house_bill_metadata()
+    #include text extraction logic later
