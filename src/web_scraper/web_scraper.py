342a78318747651096ca10fc21301e6431e22f5e
Michael DePace <michaeldepacejr@gmail.com>
2023-08-13 18:20:43 -0400

    revised code after pr review

--- src/web_scraper/web_scraper.py
+++ src/web_scraper/web_scraper.py
@@ -1,8 +1,5 @@
 """
-
 Description: this program will eventually be responsible for extracting RSS senate/house bill metadata and DOCX full bill, and then uploading to supabase table
-
-
 """
 
 from docx import Document
@@ -12,79 +9,80 @@ import feedparser
 import os
 import re
 
+from dotenv import load_dotenv
+# load_dotenv() #for local secret management with .env file
+
 senate_rss_feed = "https://www.legis.state.pa.us/WU01/LI/RSS/SenateBills.xml"
 house_rss_feed = "https://www.legis.state.pa.us/WU01/LI/RSS/HouseBills.xml"
 
-sb_api_url = "https://vsumrxhpkzegrktbtcui.supabase.co" #os.environ["API_URL"] 
-sb_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzdW1yeGhwa3plZ3JrdGJ0Y3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ3OTU2NzUsImV4cCI6MjAwMDM3MTY3NX0.9lafalZT9FJW1D8DAuIMrsRX0Gs6204nV8ETfGslrqI" #os.environ["API_KEY"] 
+sb_api_url = "https://vsumrxhpkzegrktbtcui.supabase.co"
+#sb_api_key = os.getenv("SUPABASE_API_KEY") #local secret management wtih .env file
+sb_api_key = os.environ["SUPABASE_API_KEY"] #github actions secret management 
 
-def extract_and_upload_senate_bill_metadata():
+def get_bill_no_from_RSS_title(title, leg_body):
+    senate_bill_no_regex = '(Senate Bill \d*|Senate Resolution \d*)'
+    house_bill_no_regex = '(House Bill \d*|House Resolution \d*)'
+    
+    if leg_body == "senate": return re.findall(senate_bill_no_regex, title)[0].split(' ')[-1]
+    if leg_body == "house": return re.findall(house_bill_no_regex, title)[0].split(' ')[-1]
+    return title #return the original title if legislative body not specified
+    
+def get_bill_printer_no_from_RSS_title(title):
+    printer_no_regex="Printer's Number \d*"
+    return re.findall(printer_no_regex, title)[0].split(' ')[-1]
+
+def check_for_existing_bill_supabase_records(supa_con, bill_no, printer_no, leg_body):
+    existing_bill_records = supa_con.table('Revisions').select("*", count="exact").eq("bill_id", bill_no).eq("printer_no", printer_no).eq("legislative_body", leg_body).execute()
+    if existing_bill_records.count > 0: return True
+    else: return False
+
+def extract_and_upload_senate_bill_metadata(supa_con):
     sen_rss = feedparser.parse(senate_rss_feed)
-    supabase: Client = create_client(sb_api_url, sb_api_key)
     for bill in sen_rss.entries:
-        #print("checking bill ", bill.title)
         if bill is None: continue
 
-        bill_no_regex = '(Senate Bill \d*|Senate Resolution \d*)'
-        bill_no = re.findall(bill_no_regex, bill.title)[0].split(' ')[-1]
+        bill_no = get_bill_no_from_RSS_title(bill.title, "senate")
+        printer_no = get_bill_printer_no_from_RSS_title(bill.title)
         
-        printer_no_regex="Printer's Number \d*"
-        printer_no = re.findall(printer_no_regex, bill.title)[0].split(' ')[-1]
-
-        #OLD CODE that doesn't work
-        #title_elements = bill.title.split(' ') #grabbing bill and printer numbers from the title (not explicitly given in RSS feed)
-        #bill_no = "".join(char for char in title_elements[2] if char.isdigit()) #ensuring that only the actual digits of the bill number are extracted
-        #printer_no = "".join(char for char in title_elements[5] if char.isdigit()) #ensuring that only the actual digits of the printer number are extracted
-
-        #change if bill["parss_enacted"] == "YES" and title_elements[1] == "Bill": #make sure the bill was enacted and it's a bill instead of a resolution        
-        bill_existing_records = supabase.table('Revisions').select("*", count="exact").eq("bill_id", bill_no).eq("printer_no", printer_no).eq("legislative_body", "senate").execute()
-        if bill_existing_records.count == 0: #if the bill hasn't already been recorded in the supabase table, then post it (so you don't upload duplicates)
-            response = supabase.table('Revisions').insert( #why is there no python docs for upsert?
-                {
-                    # "created_at": metadata["created_at"], #supabase already defaults to now(), so this is kind of unnecessary for now
-                    "bill_id": bill_no,
-                    "printer_no": printer_no, 
-                    "full_text_link": bill["link"], 
-                    "publication_date": bill["published"], 
-                    "legislative_body": "senate"
-                    # "active_summary_id": metadata["active_summary_id"], #leaving out foreign keys for now
-                }).execute()
-            #print(response)
+        if check_for_existing_bill_supabase_records(supa_con, bill_no, printer_no, "senate"): continue
+        supa_con.table('Revisions').insert(
+            {
+                "bill_id": bill_no,
+                "printer_no": printer_no, 
+                "full_text_link": bill["link"], 
+                "publication_date": bill["published"], 
+                "legislative_body": "senate"
+                # "active_summary_id": metadata["active_summary_id"], #leaving out foreign keys for now
+            }).execute()
     return
 
-def extract_and_upload_house_bill_metadata():
+def extract_and_upload_house_bill_metadata(supa_con):
     house_rss = feedparser.parse(house_rss_feed)
-    supabase: Client = create_client(sb_api_url, sb_api_key)
     for bill in house_rss.entries:
-        #print("checking bill ", bill.title)
         if bill is None: continue
 
-        bill_no_regex = '(House Bill \d*|House Resolution \d*)'
-        bill_no = re.findall(bill_no_regex, bill.title)[0].split(' ')[-1]
+        bill_no = get_bill_no_from_RSS_title(bill.title, "house")
+        printer_no = get_bill_printer_no_from_RSS_title(bill.title)
 
-        printer_no_regex="Printer's Number \d*"
-        printer_no = re.findall(printer_no_regex, bill.title)[0].split(' ')[-1]
-
-        #old code that doesn't work
-        #title_elements = bill.title.split(' ')#grabbing bill and printer numbers from the title (not explicitly given in RSS feed)
-        #bill_no = "".join(char for char in title_elements[2] if char.isdigit()) #ensuring that only the actual digits of the bill number are extracted
-        #printer_no = "".join(char for char in title_elements[5] if char.isdigit()) #ensuring that only the actual digits of the printer number are extracted
-
-        #if bill["parss_enacted"] == "YES" and title_elements[1] == "Bill": #make sure the bill was enacted and it's a bill instead of a resolution
-        bill_record = supabase.table('Revisions').select("*", count="exact").eq("bill_id", bill_no).eq("printer_no", printer_no).eq("legislative_body", "house").execute()
-        if bill_record.count == 0: #if the bill hasn't already been recorded in the supabase table, then post it (so you don't upload duplicates)
-            response = supabase.table('Revisions').insert( #why is there no python docs for upsert?
-                {
-                    # "created_at": metadata["created_at"], #supabase already defaults to now(), so this is kind of unnecessary for now
-                    "bill_id": bill_no,
-                    "printer_no": printer_no, 
-                    "full_text_link": bill["link"], 
-                    "publication_date": bill["published"], 
-                    "legislative_body": "house"
-                    # "active_summary_id": metadata["active_summary_id"], # "active_summary_id": metadata["active_summary_id"], #leaving out foreign keys for now
-                }).execute()
-            #print(response)
+        if check_for_existing_bill_supabase_records(supa_con, bill_no, printer_no, "house"): continue
+        supa_con.table('Revisions').insert(
+            {
+                "bill_id": bill_no,
+                "printer_no": printer_no, 
+                "full_text_link": bill["link"], 
+                "publication_date": bill["published"], 
+                "legislative_body": "house"
+                # "active_summary_id": metadata["active_summary_id"],  #leaving out foreign keys for now
+            }).execute()
     return
+
+
+
+
+
+
+
+
 
 #
 # THE COMMENTED OUT CODE BELOW WORKS, but is on hold until the doc to docx conversion process has been implemented
@@ -119,7 +117,15 @@ def process_docx_files(): #function to handle the scraping and uploading of all 
     return
 
 
+
+
+
+
+
+
 if __name__ == "__main__":
-    extract_and_upload_senate_bill_metadata()
-    extract_and_upload_house_bill_metadata()
+    # print(sb_api_key)
+    supabase_connection: Client = create_client(sb_api_url, sb_api_key)
+    extract_and_upload_senate_bill_metadata(supabase_connection)
+    extract_and_upload_house_bill_metadata(supabase_connection)
     #include text extraction logic later
