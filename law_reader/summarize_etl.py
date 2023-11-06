1baf3daaf0bf362a4c26ec64a428d64cbd74917c
maxachis <maxachis@gmail.com>
2023-11-06 09:19:30 -0500

    Update summarize_etl to correctly insert data, added affiliated class

--- law_reader/summarize_etl.py
+++ law_reader/summarize_etl.py
@@ -1,6 +1,8 @@
 from supabase import create_client, Client
 import os
 
+from common.RevisionSummaryInfo import RevisionSummaryInfo
+
 """
 File for downloading full text of bills from Supabase, summarizing them, and uploading the summaries to Supabase
 
@@ -20,29 +22,16 @@ sb_api_url = os.environ["SUPABASE_API_URL"]  # github actions secret management
 sb_api_key = os.environ["SUPABASE_API_KEY"]  # github actions secret management
 
 
-
-def get_revisions_without_summaries(supabase_connection: Client) -> list:
+def get_revisions_without_summaries(supabase_connection: Client) -> list[RevisionSummaryInfo]:
     """
     Pulls all revisions from Supabase that do not have a summary
     :param supabase_connection: a Supabase connection object
     :return: a list of revisions without summaries
     """
     api_response = supabase_connection.table("Revisions").select("*", count="exact").is_("active_summary_id", "NULL").execute()
-    # TODO: Figure out how to get the result as a list
-    return [entry["revision_guid"] for entry in api_response.data]
-
-
-def download_bill_text(supabase_connection: Client, revision_guid: str) -> str:
-    """
-    Downloads the full text of a bill from Supabase
-    :param supabase_connection: a Supabase connection object
-    :param revision_guid: the guid of the bill revision
-    :return: the full text of the bill
-    """
-    # TODO: Add section where you get the revision_internal_id from the Revisions table, or else have the function take in the revision_internal_id
-    api_response = supabase_connection.table("Revision_Text").select("full_text").eq("revision_guid", revision_guid).execute()
-    return api_response.data[0]["full_text"]
-
+    # Put relevant information from each entry into a RevisionTextInfo object
+    results = [RevisionSummaryInfo(entry["revision_guid"], entry["rt_unique_id"], entry["revision_internal_id"]) for entry in api_response.data]
+    return results
 
 def summarize_bill(bill_text: str) -> str:
     """
@@ -50,10 +39,20 @@ def summarize_bill(bill_text: str) -> str:
     :param bill_text: the text of the bill
     :return: the summary of the bill
     """
-    return bill_text
+    return "This is a placeholder summary."
 
 
-def upload_summary(supabase_connection: Client, summary: str, revision_guid: str):
+def download_bill_text(supabase_connection: Client, rt_unique_id: int) -> str:
+    """
+    Downloads the full text of a bill from Supabase
+    :param supabase_connection: a Supabase connection object
+    :param rt_unique_id: the unique id of the bill in the Revision_Text table
+    :return: the full text of the bill
+    """
+    api_response = supabase_connection.table("Revision_Text").select("full_text").eq("rt_unique_id", rt_unique_id).execute()
+    return api_response.data[0]["full_text"]
+
+def upload_summary(supabase_connection: Client, revision_info: RevisionSummaryInfo):
     """
     Uploads a summary to Supabase, linking it to the appropriate entry in the "Revisions" table
     :param supabase_connection: a Supabase connection object
@@ -61,10 +60,16 @@ def upload_summary(supabase_connection: Client, summary: str, revision_guid: str
     :param revision_guid: the guid of the bill revision
     """
     # Insert summary into Summaries table and retrieve the summary_id of the new entry
-    api_response = supabase_connection.table("Summaries").insert({"summary": summary}).execute()
+    api_response = supabase_connection.table("Summaries").insert(
+        {"summary_text": revision_info.summary,
+         "revision_internal_id": revision_info.revision_internal_id}).\
+        execute()
     summary_id = api_response.data[0]["summary_id"]
     # Update relevant entry in Revisions table (found with revision guid) with summary_id of new entry in Summaries table
-    supabase_connection.table("Revisions").update({"active_summary_id": summary_id}).eq("revision_guid", revision_guid).execute()
+    supabase_connection.table("Revisions").\
+        update({"active_summary_id": summary_id}).\
+        eq("revision_guid", revision_info.revision_guid).\
+        execute()
 
 
 def summarize_all_unsummarized_revisions(supabase_connection: Client):
@@ -73,10 +78,10 @@ def summarize_all_unsummarized_revisions(supabase_connection: Client):
     :param supabase_connection: a Supabase connection object
     """
     revisions_without_summaries = get_revisions_without_summaries(supabase_connection)
-    for revision_guid in revisions_without_summaries:
-        bill_text = download_bill_text(supabase_connection, revision_guid)
-        summary = summarize_bill(bill_text)
-        upload_summary(supabase_connection, summary, revision_guid)
+    for revision_info in revisions_without_summaries:
+        revision_info.full_text = download_bill_text(supabase_connection, revision_info.rt_unique_id)
+        revision_info.summary = summarize_bill(revision_info.full_text)
+        upload_summary(supabase_connection, revision_info)
 
 
 if __name__ == "__main__":
