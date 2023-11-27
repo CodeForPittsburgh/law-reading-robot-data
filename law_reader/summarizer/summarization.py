5039946869ec8ce9c7ec753672e321b04d36d792
Max Chis <48846180+maxachis@users.noreply.github.com>
2023-11-27 18:13:22 -0500

    Fix import bug

--- law_reader/summarizer/summarization.py
+++ law_reader/summarizer/summarization.py
@@ -21,8 +21,8 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.vectorstores import Chroma
 from langchain.embeddings import GPT4AllEmbeddings
 
-from law_reader.summarizer.SummarizationException import SummarizationException
-from law_reader.summarizer.summarizer import Summarizer
+from SummarizationException import SummarizationException
+from summarizer import Summarizer
 
 
 class Summarization(Summarizer):
