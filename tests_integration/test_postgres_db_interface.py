19ce896bb1f3795a316f67224ce9061a7154c8e5
maxachis <maxachis@gmail.com>
2024-01-15 16:32:44 -0500

    Update to account for changes to DBInterface

--- tests_integration/test_postgres_db_interface.py
+++ tests_integration/test_postgres_db_interface.py
@@ -13,11 +13,7 @@ class TestPostgresDBInterface(unittest.TestCase):
         def setUp(self):
             load_dotenv()  # Load environment variables from .env file
             # Create a PostgresDBInterface object
-            self.db_interface = PostgresDBInterface(
-                db_password=os.environ.get("SUPABASE_DB_PASSWORD"),
-                db_host=os.environ.get("SUPABASE_DB_HOST"),
-                port=os.environ.get("SUPABASE_DB_PORT")
-            )
+            self.db_interface = PostgresDBInterface()
             # Truncate the tables in the database
             for table in ["Bills", "Revisions", "Revision_Text", "Summaries"]:
                 self.db_interface.execute("TRUNCATE TABLE \"{}\" CASCADE".format(table))
